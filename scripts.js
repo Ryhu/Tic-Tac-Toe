28e1ae322dad1179ff8bdd8212dc19ba4e182e77
ryan <ryanhuuu@gmail.com>
2018-02-12 21:55:52 -0500

    mostly coded on codepen, needs fixing

---
+++ scripts.js
@@ -0,0 +1,100 @@
+button:focus {outline:0 !important;}
+
+#slot{
+  margin-top:200px;
+}
+
+#grid{
+  background-color:white;
+  height:310px;
+  width:309px;
+  margin-top:50px;
+}
+
+.menu{
+  background-color:white;
+  height:310px;
+  width:309px;
+  margin-top:50px;
+}
+
+.menuButton{
+  color:black;
+  background-color:white;
+  height:100px;
+  width:309px;
+  margin-bottom:2px;
+  margin-top:2px;
+  font-size:50px;
+  border: none;
+}
+.highlight:hover{
+  color:white;
+  background-color:black;
+  span{
+    color:white;
+    background-color:black;
+  }
+}
+
+.symbolButton{
+  color:white;
+  background-color:black;
+}
+.symHighlight:hover{
+  color:black;
+  background-color:white;
+  span{
+    color:black;
+    background-color:white;
+  }
+}
+
+
+.space{
+  color:white;
+  background-color:black;
+  height:100px;
+  width:100px;
+  margin:2px;
+
+  font-size:60px;
+  display:block;
+}
+
+.winBanner{
+  color:white;
+  background-color:black;
+  height:308px;
+  width:308px;
+  margin:2px;
+  font-size:60px;
+}
+
+
+
+.spaceRow{
+  width:400px;
+}
+
+
+.text{
+  border: none;
+  color:black;
+  font-size:30px;
+  background-color:white;
+  font-weight:bold;
+}
+.flip{
+  color:white;
+  background-color:black;
+}
+
+span{
+  font-size:70px;
+  color:white;
+}
+
+.gone{
+  display:none;
+}
